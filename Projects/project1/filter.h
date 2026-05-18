87e8e0fb7c29b019a3e2c041e6661a74c186c00e
srinivasan.shrim <srinivasan.shrim@northeastern.edu>
2026-05-18 17:11:41 -0400

    Fix author names; add LaTeX report with required images 2-6 (Tasks 4-9)

--- Projects/project1/filter.h
+++ Projects/project1/filter.h
@@ -1,6 +1,6 @@
 /*
  * CS5330 Computer Vision - Project 1
- * Authors: [Your Name], Shyam S (shyams612)
+ * Authors: Shriman Raghav Srinivasan, Shyam Srinivasan
  * Date: May 2026
  *
  * Purpose: Declares all image processing and filter functions for Project 1.
